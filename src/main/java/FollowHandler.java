61b0e47c03501044f8b040ace0a9f2e670e536e8
halileohalilei <kayimhalil@hotmail.com>
2016-09-25 11:34:41 +0300

    added response stream to follow

--- src/main/java/FollowHandler.java
+++ src/main/java/FollowHandler.java
@@ -2,6 +2,7 @@ import com.sun.net.httpserver.HttpExchange;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 /**
@@ -15,10 +16,17 @@ public class FollowHandler extends BaseHttpHandler {
         String userID = params.get("userID");
         String starID = params.get("starID");
 
-        FileWriter pw = new FileWriter("F:\\connections.csv",true);
-        pw.append(userID).append(",").append(starID);
+        FileWriter pw = new FileWriter("connections.csv",true);
+        pw.append(userID).append(",").append(starID).append('\n');
         pw.flush();
         pw.close();
         RecommendationServer.trainRecommender();
+
+        String response = userID + " followed " + starID;
+
+        httpExchange.sendResponseHeaders(200, response.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
--- src/main/java/RecommendationServer.java
+++ src/main/java/RecommendationServer.java
@@ -1,12 +1,10 @@
 import com.sun.net.httpserver.HttpServer;
 import org.apache.mahout.cf.taste.impl.model.MemoryIDMigrator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
-import org.apache.mahout.cf.taste.impl.model.jdbc.GenericJDBCDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefUserBasedRecommender;
 import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
-import org.apache.mahout.cf.taste.model.JDBCDataModel;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
